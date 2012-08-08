438f6f79ceba9ca649395a47cfa2a1423b8647c5
Mike Pennisi <mike@mikepennisi.com>
2012-08-08 18:33:28 -0400

    Slight improvement to Twitter.com userscript

--- .userscripts/twitter.equalizer.user.js
+++ .userscripts/twitter.equalizer.user.js
@@ -10,9 +10,13 @@
 (function(window, undefined) {
 	var text = [];
 	var styleNode;
+	// Obscure stats for users (but imply their presence with a border)
 	text.push(".stats { color: transparent;border: dashed 1px #555; }");
 	text.push(".stats * { color: transparent; }");
 	text.push(".stats img { display: none; }");
+	// Individual tweet stats are displayed conditionally, so they should be
+	// hidden outright.
+	text.push(".tweet-stats-container { display: none; }");
 	text = text.join(" ").replace(/\s*;/g, " !important;");
 	styleNode = document.createElement("style");
 	styleNode.innerText = text;
